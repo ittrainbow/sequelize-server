d170cdc42191374b59a0ed06f8c2005471667476
Andrew Gordienko <50226581+ittrainbow@users.noreply.github.com>
2023-11-25 23:36:15 +0300

    error handling 2

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,11 @@
 const express = require('express')
 
-const appRouter = require('./appRouter')
 const usersRouter = require('./usersRouter')
 const ticketsRouter = require('./ticketsRouter')
 const projectsRouter = require('./projectsRouter')
 
 const router = express.Router()
 
-router.use('/', appRouter)
 router.use('/users', usersRouter)
 router.use('/tickets', ticketsRouter)
 router.use('/projects', projectsRouter)
