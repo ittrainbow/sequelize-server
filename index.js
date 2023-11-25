a5e807ce3f473e966fd1a0f32a8b27e12155170e
Andrew Gordienko <50226581+ittrainbow@users.noreply.github.com>
2023-11-26 02:38:59 +0300

    app headers d

--- index.js
+++ index.js
@@ -25,15 +25,16 @@ const models = require('./models')
 const router = require('./routes')
 app.use(
   cors({
-    origin: '*'
+    credentials: true,
+    origin: true
   })
 )
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST')
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
-  next()
-})
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', '*')
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST')
+//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+//   next()
+// })
 app.use(express.json())
 app.get('/', (req, res) => res.status(200).json({ message: 'app is up!' }))
 app.use('/api', router)
